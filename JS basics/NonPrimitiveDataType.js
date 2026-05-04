646eb75244c107df926fe085ed881287c999e4d5
aforarpitkumar <80710050+aforarpitkumar@users.noreply.github.com>
2026-05-04 05:12:20 +0000

    JS commit 3 NonPrimitive data Types

--- JS basics/NonPrimitiveDataType.js
+++ JS basics/NonPrimitiveDataType.js
@@ -14,3 +14,8 @@ Details.Alive = true; // we can add more variables inside a constant object
 console.log(typeof(Details));
 console.log(Details.Name);
 console.log(Details.Alive);
+
+let ArrayGroup = [ "A", "B", true];
+
+console.log(ArrayGroup);
+console.log(typeof(ArrayGroup));
