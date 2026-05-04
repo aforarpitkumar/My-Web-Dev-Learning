4e18f3f758a94dcba13843780591ec7faaec7a5e
aforarpitkumar <80710050+aforarpitkumar@users.noreply.github.com>
2026-05-04 04:52:48 +0000

    NonPrimitivedatatypes Addition

--- JS basics/DataTypes.js
+++ JS basics/DataTypes.js
@@ -7,3 +7,7 @@
  console.log(name);
  console.log(age);
  console.log(alive);
+
+ console.log(typeof(alive));
+  console.log(typeof(age));
+   console.log(typeof(name));
---
+++ JS basics/NonPrimitiveDataType.js
@@ -0,0 +1,16 @@
+let username = {};
+
+console.log(username);
+console.log(typeof username);
+
+const Details = {
+
+    Name : "Arpit",
+    age :26
+}
+
+Details.Alive = true; // we can add more variables inside a constant object
+
+console.log(typeof(Details));
+console.log(Details.Name);
+console.log(Details.Alive);
