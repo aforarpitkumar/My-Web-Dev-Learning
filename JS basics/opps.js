591700bd4b75018afb78fb3327065da5f9bbce1c
aforarpitkumar <80710050+aforarpitkumar@users.noreply.github.com>
2026-05-11 06:06:07 +0000

    example of encapsulation in JS 2

--- JS basics/opps.js
+++ JS basics/opps.js
@@ -83,10 +83,14 @@ class BankAccount {
         this.accountHolderName = accountHolderName;
     }
 
+    // this function will add the amount to the balance and return a message of successful deposit along with the new balance
+
     deposit(amount) {
         this.#balance += amount;
         return "Deposit successful. New balance: " + this.#balance;
     }
+
+    // this function will check if the amount to be withdrawn is greater than the current balance or not, if it is greater then it will return a message of insufficient funds and if it is less than or equal to the current balance then it will deduct the amount from the balance and return a message of successful withdrawal along with the new balance
 
     withdraw(amount) {
         if (amount > this.#balance) {
@@ -97,16 +101,28 @@ class BankAccount {
         }
     }
 
+    // this function will return the account details like account number, account holder name and current balance
+
     getAccountDetails() {
         return "Account Number: " + this.accountNumber + ", Account Holder: " + this.accountHolderName + ", Balance: " + this.#balance;
     }
 }
 
 let myAccount = new BankAccount("123456789", "Arpit Kumar");
+let myAccount2 = new BankAccount("987654321", "John Doe");
 
 console.log(myAccount.getAccountDetails());
 console.log(myAccount.deposit(2000));
 console.log(myAccount.withdraw(3000));
 console.log(myAccount.withdraw(5000));
+console.log(myAccount.withdraw(1000));
+console.log(myAccount.getAccountDetails());
+
+console.log("Now we will check the details of the second account which is created by John Doe");
+
+console.log(myAccount2.getAccountDetails());
+console.log(myAccount2.deposit(5000));
+console.log(myAccount2.withdraw(2000));
+console.log(myAccount2.getAccountDetails());
 
 
